802d44db29f5a3e59ced3e48781e304ad0b314d3
xtokio <xtokio@gmail.com>
2023-08-16 10:41:26 -0400

    Fix to work with latest version of Crystal DB shard

--- src/odpi/driver.cr
+++ src/odpi/driver.cr
@@ -2,6 +2,11 @@ class ODPI::Driver < DB::Driver
   def build_connection(context : DB::ConnectionContext) : ODPI::Connection
     ODPI::Connection.new(context)
   end
+
+  def connection_builder(uri : URI) : DB::ConnectionBuilder
+    # Error fix
+    # Error: abstract `def DB::Driver#connection_builder(uri : URI)` must be implemented by ODPI::Driver
+  end
 end
 
 DB.register_driver "odpi", ODPI::Driver
